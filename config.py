40f2b6e4ae4fc3b648d49ca2e2f85055d708b51e
Ilya Fitsula <ilya.fitsula@eleks.com>
2016-11-23 14:05:31 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,5 @@
+# Token
+token = ''
+
+# Access list
+name = ''
---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+import telebot
+import config
+
+bot = telebot.TeleBot(config.token)
+
+# message.text = text from user
+# message = json from user message
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message):
+    chat = subprocess.check_output(message.text, shell=True)
+#    chat = os.system('sudo lsof -Pni:80')
+    if message.from_user.first_name == config.name:
+        bot.send_message(message.chat.id, chat)
+    else:
+        bot.send_message(message.chat.id, 'Good try, but no')
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
